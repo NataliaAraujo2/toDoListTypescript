67cf7caf585cd1412dbc6645e952b9cfd5e7d0e3
Natália de Araujo Nogueira <nataliaanog@gmail.com>
2024-03-31 15:01:45 -0300

    .

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>React + TS Todo</h1>
+      </header>
+      <h1>Conteúdo...</h1>
+      <footer>
+        <p><span>React + TS</span>@2023</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
